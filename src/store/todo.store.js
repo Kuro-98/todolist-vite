1e62ec3c814afb92dc1da8dc004c9e19fb7306f8
Emanuel Morales <101947718+Kuro-98@users.noreply.github.com>
2024-05-13 17:11:22 -0600

    listeners added

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,12 +20,71 @@ const loadStrore = () => {
 	throw new Error('Not implemented');
 };
 
+const getTodos = (filter = Filters.All) => {
+	switch (filter) {
+		case Filters.All:
+			return [...state.todos];
+		case Filters.Completed:
+			return state.todos.filter((todo) => todo.done);
+		case Filters.Pending:
+			return state.todos.filter((todo) => !todo.done);
+		default:
+			throw new Error(`Option ${filter} is not valid.`);
+	}
+};
+
+/**
+ *
+ * @param {String} description
+ */
+
 const addTodo = (description) => {
+	if (!description) throw new Error('Description is required');
 	const newTodo = new Todo(description);
 	state.todos.push(newTodo);
 };
 
+/**
+ *
+ * @param {String} todoId Todo identifier
+ */
+const toggleTodo = (todoId) => {
+	state.todos = state.todos.map((todo) => {
+		if (todo.id === todoId) {
+			todo.done = !todo.done;
+		}
+		return todo;
+	});
+};
+
+const deleteTodo = (todoId) => {
+	state.todos = state.todos.filter((todo) => todo.id !== todoId);
+};
+
+const deleteCompleted = () => {
+	state.todos = state.todos.filter((todo) => todo.done);
+};
+
+/**
+ *
+ * @param {Filters} newFilter
+ */
+const setFilter = (newFilter = Filters.All) => {
+	state.filter = newFilter;
+};
+
+const getCurrentFilter = () => {
+	return state.filter;
+};
+
 export default {
+	addTodo,
+	deleteCompleted,
+	deleteTodo,
+	getCurrentFilter,
 	initStore,
+	getTodos,
 	loadStrore,
+	setFilter,
+	toggleTodo,
 };
--- src/todos/app.js
+++ src/todos/app.js
@@ -1,4 +1,11 @@
+import todoStore from '../store/todo.store';
 import html from './app.html?raw';
+import { renderTodos } from './use-cases';
+
+const ElementIDs = {
+	TodoList: '.todo-list',
+	NewTodoInput: '#new-todo-input',
+};
 
 /**
  *
@@ -6,9 +13,32 @@ import html from './app.html?raw';
  */
 
 export const App = (elementId) => {
+	const displayTodos = () => {
+		const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+		renderTodos(ElementIDs.TodoList, todos);
+	};
+
 	(() => {
 		const app = document.createElement('div');
 		app.innerHTML = html;
 		document.querySelector(elementId).appendChild(app);
+		displayTodos();
 	})();
+
+	const newDescriptionInput = document.querySelector(ElementIDs.NewTodoInput);
+	const todoListUL = document.querySelector(ElementIDs.TodoList);
+
+	newDescriptionInput.addEventListener('keyup', (event) => {
+		if (event.keyCode !== 13) return;
+		if (event.target.value.trim().length === 0) return;
+		todoStore.addTodo(event.target.value);
+		event.target.value = '';
+		displayTodos();
+	});
+
+	todoListUL.addEventListener('clcik', (event) => {
+		const element = event.target.closest('[data-id]');
+		todoStore.toggleTodo(element.getAttribute('data-id'));
+		displayTodos();
+	});
 };
